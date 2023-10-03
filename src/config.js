73a7b404a0ebc028b40fe9d6f1a808ada905c813
Jordy Alejandro <jamaza4@espe.edu.ec>
2023-10-02 19:59:50 -0500

    second commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 4000;
+export const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb+srv://jamaza:WM3OBQQtvUzMzugz@cluster0.9mjhgh2.mongodb.net/mern-tasks";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
